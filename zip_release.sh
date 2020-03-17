2a32ddebb9cc3e78136b81cb89a1731c02b31375
DavidM42 <davidmerz@gmx.de>
2020-03-18 00:25:53 +0100

    added icons in other sizes and version bump, less permissions

--- zip_release.sh
+++ zip_release.sh
@@ -1,2 +1,2 @@
-zip -r -FS ../CovidFreeWeb.zip * -x *.git/* -x node_modules/\* -x .vscode/* -x *.md -x *.sh -x assets/\*
+zip -r -FS ../CovidFreeWeb.zip * -x *.git/* -x node_modules/\* -x .vscode/* -x *.md -x *.sh -x assets/\* -x web-ext-artifacts/\*
 # mv ../CovidFreeWeb.zip ../CovidFreeWeb.xpi
